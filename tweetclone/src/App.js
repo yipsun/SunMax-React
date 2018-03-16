4852bfae13c296ed88933a869649b963b262c561
sonda <rhys@llamamedia.ca>
2018-03-16 17:12:58 -0400

    fetching data from firebase

--- tweetclone/src/App.js
+++ tweetclone/src/App.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
+import firebase from "firebase";
 import './App.css';
-import Card from './Card';
 import data from './data.json';
+
+import Card from './Card';
 
 
 class App extends Component {
-    render() {
+    
+    constructor(props) {
+        super(props);
+        this.state = { };
 
-        const cards = data.tweet.map((card) => {
-            return <Card key={card.id_str} data={card} />
+
+        // define connexion to firebase project
+        var config = {
+            apiKey: "AIzaSyDxqfw0tZOlQ6ITm_w-ZfRN5DybnQa6nYI",
+            authDomain: "tweetclone-1b35e.firebaseapp.com",
+            databaseURL: "https://tweetclone-1b35e.firebaseio.com",
+            projectId: "tweetclone-1b35e",
+            storageBucket: "tweetclone-1b35e.appspot.com",
+            messagingSenderId: "1002525357163"
+        };
+        
+        firebase.initializeApp(config);
+        
+
+        // fetching firebase db
+        this.db = firebase.database();
+        
+        // fetching root reference to db
+        this.dbRef = this.db.ref();
+
+        
+    }
+
+    componentWillMount() {
+        // db.ref("tweet") == db.ref().child("tweet")
+        this.dbRef.child("tweet").on('value', (snapshot) => {
+            this.setState( {tweet : snapshot.val()});
+        });
+
+    }
+
+
+    render() {
+        console.log(this.state);
+        const cards = this.state.tweet.map((tweet) => {
+            return <Card key={tweet.id_str} data={tweet} />
         })
 
         return (
--- tweetclone/src/Card.js
+++ tweetclone/src/Card.js
@@ -9,14 +9,14 @@ class Card extends Component {
 
     render() {
 
-        const date = moment(this.props.data.created_at).format("DD MMMM YYYY");
+        // const date = moment(this.props.data.created_at).format("DD-MMMM-YYYY");
 
         return (
             <div className="card">
                 <img src={this.props.data.user.img_url} alt="user profile pic"/>
                 <div className="card__name">{this.props.data.user.name}
                     - <span className="card__screenName">@{this.props.data.user.screen_name}</span>
-                    . <span className="card__date">{date}</span>
+                    {/* . <span className="card__date">{date}</span> */}
                 </div>
                 <p className="card__text">{this.props.data.text}</p>
             </div>
